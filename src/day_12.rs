6d53b1e3e85414ee671b6508ea78d6b444d110cc
thisdotrob <this.rob@protonmail.com>
2024-01-13 20:51:44 +0000

    Day 12 pt 2

--- src/day_12.rs
+++ src/day_12.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::collections::HashMap;
 
 pub fn run() {
     let contents = fs::read_to_string("12.txt").unwrap();
@@ -6,67 +7,55 @@ pub fn run() {
     println!("pt2: {}", pt2(&contents));
 }
 
-fn valid(springs: &Vec<char>, pattern: &Vec<usize>) -> bool {
-    let mut broken_count = 0;
-    let mut pattern_index = 0;
-
-    for char in springs {
-        let char = *char;
-        if char == '#' {
-            if pattern_index == pattern.len() {
-                return false
-            }
-            broken_count += 1;
-        } else if broken_count > 0 { 
-            if broken_count != pattern[pattern_index] {
-                return false
-            }
-
-            broken_count = 0;
-            pattern_index += 1;
-        }
+fn valid(pattern: &Vec<usize>, pattern_index: usize, broken_count: usize) -> bool {
+    if pattern_index == pattern.len() && broken_count == 0 {
+        return true
     }
 
-    if broken_count > 0 && pattern_index < pattern.len() {
-        if broken_count != pattern[pattern_index] {
-            return false
-        }
-
-        broken_count = 0;
-        pattern_index += 1;
+    if pattern_index == pattern.len() - 1 {
+        return pattern[pattern_index] == broken_count
     }
 
-    broken_count == 0 && pattern_index == pattern.len()
+    false
 }
 
-fn score(springs: &Vec<char>, pattern: &Vec<usize>, index: usize) -> usize {
-    if index == springs.len() {
-        if valid(springs, pattern) {
-            return 1
+fn score(springs: &Vec<char>, pattern: &Vec<usize>, memo_key: [usize; 3], memo: &mut HashMap<[usize; 3], usize>) -> usize {
+    if let Some(answer) = memo.get(&memo_key) {
+        return *answer
+    }
+
+    let [springs_index, pattern_index, broken_count] = memo_key;
+
+    if springs_index == springs.len() {
+        if valid(pattern, pattern_index, broken_count) {
+            return 1;
         } else {
-            return 0
-        }
-    } else {
-        let char = springs[index];
-
-        if char == '?' {
-            let mut answer = 0;
-
-            let mut springs1 = springs.clone();
-            springs1[index] = '#';
-
-            answer += score(&mut springs1, &pattern, index + 1);
-
-            let mut springs2 = springs.clone();
-            springs2[index] = '.';
-
-            answer += score(&mut springs2, &pattern, index + 1);
-
-            return answer
-        } else {
-            return score(springs, &pattern, index + 1);
+            return 0;
         }
     }
+
+    let mut answer = 0;
+
+    let spring = springs[springs_index];
+
+    if spring == '.' || spring == '?' {
+        if broken_count == 0 {
+            let next_memo_key = [springs_index + 1, pattern_index, broken_count];
+            answer += score(springs, pattern, next_memo_key, memo);
+        } else if pattern_index < pattern.len() && pattern[pattern_index] == broken_count {
+            let next_memo_key = [springs_index + 1, pattern_index + 1, 0];
+            answer += score(springs, pattern, next_memo_key, memo);
+        }
+    }
+
+    if spring == '#' || spring == '?' {
+        let next_memo_key = [springs_index + 1, pattern_index, broken_count + 1];
+        answer += score(springs, pattern, next_memo_key, memo);
+    }
+
+    memo.insert(memo_key, answer);
+
+    answer
 }
 
 fn pt1(contents: &str) -> usize {
@@ -74,18 +63,35 @@ fn pt1(contents: &str) -> usize {
 
     for line in contents.lines() {
         let (springs, pattern) = line.split_once(' ').unwrap();
-        let mut springs: Vec<_> = springs.chars().collect();
+        let springs: Vec<_> = springs.chars().collect();
         let pattern = pattern.split(',').map(|s| s.parse::<usize>().unwrap()).collect();
-        let s = score(&mut springs, &pattern, 0);
-        // println!("{} = {}", line, s);
-        answer += s;
+
+        let mut memo = HashMap::new();
+        
+        answer += score(&springs, &pattern, [0, 0, 0], &mut memo);
     }
 
     answer
 }
 
 fn pt2(contents: &str) -> usize {
-    0
+    let mut answer = 0;
+
+    for line in contents.lines() {
+        let (springs, pattern) = line.split_once(' ').unwrap();
+
+        let springs = [springs; 5].join("?");
+        let springs: Vec<_> = springs.chars().collect();
+
+        let pattern = [pattern; 5].join(",");
+        let pattern = pattern.split(',').map(|s| s.parse::<usize>().unwrap()).collect();
+
+        let mut memo = HashMap::new();
+
+        answer += score(&springs, &pattern, [0, 0, 0], &mut memo);
+    }
+
+    answer
 }
 
 #[cfg(test)]
@@ -103,48 +109,21 @@ mod day_12_pt_1_tests {
 
         assert_eq!(21, pt1(example_input));
     }
-
-    #[test]
-    fn test_score_for_last_line_of_example_input() {
-        let springs = vec!['?', '#', '#', '#', '?', '?', '?', '?', '?', '?', '?', '?'];
-        let pattern = vec![3, 2, 1];
-
-        assert_eq!(10, score(&springs, &pattern, 0));
-    }
-
-    #[test]
-    fn test_valid() {
-        let pattern = vec![3, 2, 1];
-
-        let invalid_permutations = vec![
-            vec!['.', '#', '#', '#', '.', '#', '#', '.', '#', '.', '#', '#'],
-            vec!['.', '#', '#', '#', '.', '#', '#', '.', '#', '.', '.', '#'],
-            vec!['.', '#', '#', '#', '.', '#', '#', '.', '.', '#', '.', '#'],
-            vec!['.', '#', '#', '#', '.', '.', '#', '#', '.', '#', '.', '#'],
-            vec!['.', '#', '#', '#', '.', '.', '.', '.', '.', '.', '#', '#'],
-        ];
-
-        let valid_permutations = vec![
-            vec!['.', '#', '#', '#', '.', '#', '#', '.', '#', '.', '.', '.'],
-            vec!['.', '#', '#', '#', '.', '#', '#', '.', '.', '#', '.', '.'],
-            vec!['.', '#', '#', '#', '.', '#', '#', '.', '.', '.', '#', '.'],
-            vec!['.', '#', '#', '#', '.', '#', '#', '.', '.', '.', '.', '#'],
-            vec!['.', '#', '#', '#', '.', '.', '#', '#', '.', '#', '.', '.'],
-            vec!['.', '#', '#', '#', '.', '.', '#', '#', '.', '.', '#', '.'],
-            vec!['.', '#', '#', '#', '.', '.', '#', '#', '.', '.', '.', '#'],
-            vec!['.', '#', '#', '#', '.', '.', '.', '#', '#', '.', '#', '.'],
-            vec!['.', '#', '#', '#', '.', '.', '.', '#', '#', '.', '.', '#'],
-            vec!['.', '#', '#', '#', '.', '.', '.', '.', '#', '#', '.', '#'],
-
-        ];
-
-        for permutation in invalid_permutations {
-            assert!(!valid(&permutation, &pattern));
-        }
-
-        for permutation in valid_permutations {
-            assert!(valid(&permutation, &pattern));
-        }
-    }
 }
 
+#[cfg(test)]
+mod day_12_pt_2_tests {
+    use super::*;
+
+    #[test]
+    fn test_example_input() {
+        let example_input = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1";
+
+        assert_eq!(525152, pt2(example_input));
+    }
+}
