c030b3db5da30ff9e5a27e175c89f5ac9bec17df
thisdotrob <this.rob@protonmail.com>
2024-01-15 18:02:30 +0000

    Day 13 pt 2

--- src/day_13.rs
+++ src/day_13.rs
@@ -16,6 +16,62 @@ fn points_of_reflection(pattern: &Vec<Vec<char>>) -> (Option<usize>, Option<usiz
     (horizontal_point_of_reflection, vertical_point_of_reflection)
 }
 
+fn points_of_reflection_with_smudge(pattern: &Vec<Vec<char>>) -> Vec<usize> {
+    let mut points_of_reflection = vec![];
+
+    let lookup = row_reflection_key_lookup(&pattern);
+
+    for row_index in 0..lookup.len() - 1 {
+        let mut i0 = row_index;
+        let mut i1 = row_index + 1;
+
+        let mut smudge_seen = false;
+
+        loop {
+            let key0 = &lookup[i0];
+            let key1 = &lookup[i1];
+
+            let mut difference_count = reflection_key_difference_count(key0, key1);
+
+            if !smudge_seen && difference_count == 1 {
+                smudge_seen = true;
+                difference_count = 0;
+            }
+
+            if difference_count == 0 {
+                if i0 == 0 || i1 == lookup.len() - 1 {
+                    points_of_reflection.push(row_index);
+                    break;
+                } else {
+                    i0 -= 1;
+                    i1 += 1;
+                }
+            } else {
+                break;
+            }
+        }
+    }
+
+    points_of_reflection
+}
+
+fn reflection_key_difference_count(reflection_key_1: &String, reflection_key_2: &String) -> usize {
+    let mut count = 0;
+
+    let reflection_key_1: Vec<_> = reflection_key_1.chars().collect();
+    let reflection_key_2: Vec<_> = reflection_key_2.chars().collect();
+
+    let length = reflection_key_1.len();
+
+    for i in 0..length {
+        if reflection_key_1[i] != reflection_key_2[i] {
+            count += 1;
+        }
+    }
+
+    count
+}
+
 fn pt1(contents: &str) -> usize {
     let mut answer = 0;
 
@@ -37,8 +93,41 @@ fn pt1(contents: &str) -> usize {
     answer
 }
 
-fn pt2(_contents: &str) -> usize {
-    let answer = 0;
+fn pt2(contents: &str) -> usize {
+    let mut answer = 0;
+
+    let patterns = parse_patterns(contents);
+
+    for pattern in patterns {
+        let (horizontal_point, vertical_point) = points_of_reflection(&pattern);
+
+        let horizontal_points_of_reflection = points_of_reflection_with_smudge(&pattern);
+
+        for point in horizontal_points_of_reflection {
+            if let Some(non_smudge_point) = horizontal_point {
+                if point == non_smudge_point {
+                    continue;
+                }
+            }
+
+            answer += 100 * (point + 1);
+        }
+
+        let transposed_pattern = transpose_pattern(&pattern);
+
+        let vertical_points_of_reflection = points_of_reflection_with_smudge(&transposed_pattern);
+
+        for point in vertical_points_of_reflection {
+            if let Some(non_smudge_point) = vertical_point {
+                if point == non_smudge_point {
+                    continue;
+                }
+            }
+
+            answer += point + 1;
+        }
+    }
+
     answer
 }
 
@@ -192,5 +281,71 @@ mod day_13_pt1_tests {
 #....#..#";
 
         assert_eq!(405, pt1(contents));
+    }
+}
+
+#[cfg(test)]
+mod day_13_pt2_tests {
+    use super::*;
+
+    #[test]
+    fn test_points_of_reflection_with_smudge() {
+        let pattern = vec![
+            vec!['#', '.', '#', '#', '.', '.', '#', '#', '.'],
+            vec!['.', '.', '#', '.', '#', '#', '.', '#', '.'],
+            vec!['#', '#', '.', '.', '.', '.', '.', '.', '#'],
+            vec!['#', '#', '.', '.', '.', '.', '.', '.', '#'],
+            vec!['.', '.', '#', '.', '#', '#', '.', '#', '.'],
+            vec!['.', '.', '#', '#', '.', '.', '#', '#', '.'],
+            vec!['#', '.', '#', '.', '#', '#', '.', '#', '.'],
+        ];
+
+        assert_eq!(vec![2], points_of_reflection_with_smudge(&pattern));
+
+        let pattern = transpose_pattern(&pattern);
+
+        assert_eq!(vec![4], points_of_reflection_with_smudge(&pattern));
+
+        let pattern = vec![
+            vec!['#', '.', '.', '.', '#', '#', '.', '.', '#'],
+            vec!['#', '.', '.', '.', '.', '#', '.', '.', '#'],
+            vec!['.', '.', '#', '#', '.', '.', '#', '#', '#'],
+            vec!['#', '#', '#', '#', '#', '.', '#', '#', '.'],
+            vec!['#', '#', '#', '#', '#', '.', '#', '#', '.'],
+            vec!['.', '.', '#', '#', '.', '.', '#', '#', '#'],
+            vec!['#', '.', '.', '.', '.', '#', '.', '.', '#'],
+        ];
+
+        assert_eq!(vec![0, 3], points_of_reflection_with_smudge(&pattern));
+
+        let pattern = transpose_pattern(&pattern);
+
+        assert_eq!(0, points_of_reflection_with_smudge(&pattern).len());
+    }
+
+    #[test]
+    fn test_pt2_first_pattern() {
+        let contents = "#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.";
+
+        assert_eq!(300, pt2(contents));
+    }
+
+    #[test]
+    fn test_pt2_second_pattern() {
+        let contents = "#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#";
+
+        assert_eq!(100, pt2(contents));
     }
 }
