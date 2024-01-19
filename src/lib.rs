b4e9d2f61852ee1d7a7a953ec2dbd6be782cf479
thisdotrob <this.rob@protonmail.com>
2024-01-19 21:53:29 +0000

    Day 18 pt 1

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ mod day_14;
 mod day_15;
 mod day_16;
 mod day_17;
+mod day_18;
 mod day_2;
 mod day_3;
 mod day_4;
@@ -66,5 +67,8 @@ pub fn run() {
     println!();
     println!("========== Day 17 ==========");
     day_17::run();
+    println!();
+    println!("========== Day 18 ==========");
+    day_18::run();
     println!();
 }
