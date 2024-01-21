fc9bba8ff1df51d9dea66d264587352418618945
thisdotrob <this.rob@protonmail.com>
2024-01-21 18:37:20 +0000

    Day 18 pt 2

--- src/day_18.rs
+++ src/day_18.rs
@@ -3,8 +3,10 @@ use std::fs;
 pub fn run() {
     let example_input = fs::read_to_string("18_example.txt").unwrap();
     println!("pt1 example: {}", pt1(&example_input));
+    println!("pt2 example: {}", pt2(&example_input));
     let input = fs::read_to_string("18.txt").unwrap();
     println!("pt1: {}", pt1(&input));
+    println!("pt2: {}", pt2(&input));
 }
 
 fn pt1(input: &str) -> i32 {
@@ -20,6 +22,45 @@ fn pt1(input: &str) -> i32 {
             "L" => (-1, 0),
             "U" => (0, -1),
             "D" => (0, 1),
+            _ => panic!("invalid direction"),
+        };
+
+        pos.0 += modifier.0 * meters;
+        pos.1 += modifier.1 * meters;
+        trench_positions.push(pos);
+        perimiter_count += meters;
+    }
+    let windows = trench_positions.windows(2).collect::<Vec<_>>();
+
+    let mut area = 0;
+
+    for window in windows {
+        let (x1, y1) = window[0];
+        let (x2, y2) = window[1];
+
+        area += (x2 - x1) * (y2 + y1);
+    }
+
+    1 + ((area.abs() + perimiter_count) / 2)
+}
+
+fn pt2(input: &str) -> i64 {
+    let mut trench_positions = vec![(0, 0)];
+    let mut perimiter_count = 0;
+    let mut pos = (0, 0);
+    for line in input.lines() {
+        let mut line = line.split(" ");
+        let _ = line.next();
+        let _ = line.next();
+        let instruction = line.next().unwrap();
+        let meters = &instruction[2..7];
+        let meters = i64::from_str_radix(meters, 16).unwrap();
+        let direction = &instruction[7..8];
+        let modifier = match direction {
+            "0" => (1, 0),
+            "2" => (-1, 0),
+            "3" => (0, -1),
+            "1" => (0, 1),
             _ => panic!("invalid direction"),
         };
 
