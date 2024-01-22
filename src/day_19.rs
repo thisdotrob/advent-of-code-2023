eba866f96f771c354aa6ba0dc8a450da9976c56c
thisdotrob <this.rob@protonmail.com>
2024-01-22 14:22:09 +0000

    Day 19 pt 2 (incorrect example input answer)

--- src/day_19.rs
+++ src/day_19.rs
@@ -4,13 +4,13 @@ use std::fs;
 pub fn run() {
     let example_input = fs::read_to_string("19_example.txt").unwrap();
     println!("pt1 example: {}", pt1(&example_input));
-    // println!("pt2 example: {}", pt2(&example_input));
+    println!("pt2 example: {}", pt2(&example_input));
     let input = fs::read_to_string("19.txt").unwrap();
     println!("pt1: {}", pt1(&input));
     // println!("pt2: {}", pt2(&input));
 }
 
-fn pt1(input: &str) -> i32 {
+fn pt1(input: &str) -> u64 {
     let (workflows, parts) = parse_input(input);
 
     let mut answer = 0;
@@ -61,8 +61,174 @@ fn pt1(input: &str) -> i32 {
     answer
 }
 
-fn pt2(input: &str) -> i32 {
-    0
+fn pt2(input: &str) -> u64 {
+    let (workflows, _) = parse_input(input);
+
+    let part_range = PartRange {
+        x: [1, 4000],
+        m: [1, 4000],
+        a: [1, 4000],
+        s: [1, 4000],
+    };
+
+    let accepted_part_ranges = apply_workflow(&part_range, &workflows, "in", 0);
+
+    let mut answer = 0;
+
+    for range in accepted_part_ranges {
+        dbg!(&range);
+        let num_distinct_combinations = (range.x[1] - range.x[0])
+            * (range.m[1] - range.m[0])
+            * (range.a[1] - range.a[0])
+            * (range.s[1] - range.s[0]);
+        answer += dbg!(num_distinct_combinations);
+    }
+
+    answer
+}
+
+fn apply_workflow(
+    part_range: &PartRange,
+    workflows: &HashMap<&str, Workflow>,
+    destination_workflow: &str,
+    rule_index: usize,
+) -> Vec<PartRange> {
+    let workflow = &workflows[destination_workflow];
+    if rule_index == workflow.rules.len() {
+        return vec![];
+    }
+
+    let rule = &workflow.rules[rule_index];
+
+    let mut result = vec![];
+
+    if let Some(matching_part_range) = &filter_part_range(part_range, rule) {
+        if rule.destination_workflow == "A" {
+            result.push(PartRange {
+                ..*matching_part_range
+            });
+        } else if rule.destination_workflow != "R" {
+            result.append(&mut apply_workflow(
+                matching_part_range,
+                workflows,
+                rule.destination_workflow,
+                0,
+            ));
+        }
+        if let Some(non_matching_part_range) =
+            &part_range_remainder(part_range, matching_part_range)
+        {
+            result.append(&mut apply_workflow(
+                non_matching_part_range,
+                workflows,
+                destination_workflow,
+                rule_index + 1,
+            ))
+        }
+    } else {
+        result.append(&mut apply_workflow(
+            part_range,
+            workflows,
+            destination_workflow,
+            rule_index + 1,
+        ))
+    }
+
+    result
+}
+
+fn filter_part_range(part_range: &PartRange, rule: &Rule) -> Option<PartRange> {
+    if let Some(condition) = &rule.condition {
+        let filtered_part_range = if condition.operator == '>' {
+            match condition.part_property {
+                'x' => PartRange {
+                    x: [condition.value + 1, part_range.x[1]],
+                    ..*part_range
+                },
+                'm' => PartRange {
+                    m: [condition.value + 1, part_range.m[1]],
+                    ..*part_range
+                },
+                'a' => PartRange {
+                    a: [condition.value + 1, part_range.a[1]],
+                    ..*part_range
+                },
+                's' => PartRange {
+                    s: [condition.value + 1, part_range.s[1]],
+                    ..*part_range
+                },
+                _ => panic!("invalid part property"),
+            }
+        } else {
+            // operator must be '<'
+            match condition.part_property {
+                'x' => PartRange {
+                    x: [part_range.x[0], condition.value - 1],
+                    ..*part_range
+                },
+                'm' => PartRange {
+                    m: [part_range.m[0], condition.value - 1],
+                    ..*part_range
+                },
+                'a' => PartRange {
+                    a: [part_range.a[0], condition.value - 1],
+                    ..*part_range
+                },
+                's' => PartRange {
+                    s: [part_range.s[0], condition.value - 1],
+                    ..*part_range
+                },
+                _ => panic!("invalid part property"),
+            }
+        };
+
+        if part_range_is_valid(&filtered_part_range) {
+            Some(filtered_part_range)
+        } else {
+            None
+        }
+    } else {
+        Some(PartRange { ..*part_range })
+    }
+}
+
+fn part_range_remainder(
+    original_part_range: &PartRange,
+    part_range: &PartRange,
+) -> Option<PartRange> {
+    let remainder = PartRange {
+        x: range_remainder(original_part_range.x, part_range.x),
+        m: range_remainder(original_part_range.m, part_range.m),
+        a: range_remainder(original_part_range.a, part_range.a),
+        s: range_remainder(original_part_range.s, part_range.s),
+    };
+
+    if part_range_is_valid(&remainder) {
+        Some(remainder)
+    } else {
+        None
+    }
+}
+
+fn range_remainder(original_range: [u64; 2], range: [u64; 2]) -> [u64; 2] {
+    if range[1] != original_range[1] && range[0] != original_range[0] {
+        panic!("both start and end of range are different");
+    }
+
+    if range[1] != original_range[1] {
+        [range[1] + 1, original_range[1]]
+    } else if range[0] != original_range[0] {
+        [original_range[0], range[0] + 1]
+    } else {
+        original_range
+    }
+}
+
+fn part_range_is_valid(part_range: &PartRange) -> bool {
+    part_range.x[0] < part_range.x[1]
+        && part_range.m[0] < part_range.m[1]
+        && part_range.a[0] < part_range.a[1]
+        && part_range.s[0] < part_range.s[1]
 }
 
 fn parse_input(input: &str) -> (HashMap<&str, Workflow>, Vec<Part>) {
@@ -160,13 +326,21 @@ struct Rule<'a> {
 struct RuleCondition {
     part_property: char,
     operator: char,
-    value: i32,
+    value: u64,
 }
 
 #[derive(Debug)]
 struct Part {
-    x: i32,
-    m: i32,
-    a: i32,
-    s: i32,
+    x: u64,
+    m: u64,
+    a: u64,
+    s: u64,
+}
+
+#[derive(Debug)]
+struct PartRange {
+    x: [u64; 2],
+    m: [u64; 2],
+    a: [u64; 2],
+    s: [u64; 2],
 }
